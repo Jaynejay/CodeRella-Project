c56e20588e52538f1cb89b0843317da93188e21e
Isuri Lokupathirage <isurim2002@gmail.com>
2025-04-26 00:20:21 +0530

    forgot pw section added

--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -53,7 +53,7 @@ const LoginPage = () => {
                     <div className="mb-4">
                       <label
                         htmlFor="username"
-                        className="block text-left mb-1"
+                        className="block mb-1"
                       >
                         Username:
                       </label>
@@ -63,7 +63,7 @@ const LoginPage = () => {
                         name="username"
                         value={loginData.username}
                         onChange={handleChange}
-                        className="w-full border border-gray-300 rounded-md px-3 py-2"
+                        className="text-center w-full border border-gray-300 rounded-md px-3 py-2"
                         required
                       />
                     </div>
@@ -71,7 +71,7 @@ const LoginPage = () => {
                     <div className="mb-4">
                       <label
                         htmlFor="password"
-                        className="block text-left mb-1"
+                        className="block mb-1"
                       >
                         Password:
                       </label>
@@ -81,7 +81,7 @@ const LoginPage = () => {
                         name="password"
                         value={loginData.password}
                         onChange={handleChange}
-                        className="w-full border border-gray-300 rounded-md px-3 py-2"
+                        className="text-center w-full border border-gray-300 rounded-md px-3 py-2"
                         required
                       />
                     </div>
