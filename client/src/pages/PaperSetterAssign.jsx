b093a4fb27f8470b54c2c960edea03cd81562601
Imashi Wimalasekera <wimalasekerakdisds.22@uom.lk>
2025-04-27 23:35:09 +0530

    2 pages added to exam

--- client/src/pages/PaperSetterAssign.jsx
+++ client/src/pages/PaperSetterAssign.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 import { useEffect } from "react";
