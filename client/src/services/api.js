dc5ac1ad5a195e62975e90aac13213bc1a35e45e
Isuri Lokupathirage <isurim2002@gmail.com>
2025-05-12 20:39:14 +0530

    Delete client/src/services directory

--- client/src/services/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const API_BASE_URL = "http://localhost:8080/api/users";
-
-export const getUsers = async () => {
-  const response = await axios.get(API_BASE_URL);
-  return response.data;
-};
