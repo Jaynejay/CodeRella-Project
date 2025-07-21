7d2088a1b47c5b17b5816943ab253ebcf5f8191d
Isuri Lokupathirage <isurim2002@gmail.com>
2025-07-21 13:32:35 +0530

    Update CoderellaApplication.java

--- server/src/main/java/com/example/coderella/CoderellaApplication.java
+++ server/src/main/java/com/example/coderella/CoderellaApplication.java
@@ -1,8 +1,16 @@
 package com.example.coderella;
 
+import com.example.coderella.entity.Role;
+import com.example.coderella.entity.User;
+import com.example.coderella.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableScheduling
 @SpringBootApplication
 public class CoderellaApplication {
 
@@ -10,4 +18,19 @@ public class CoderellaApplication {
 		SpringApplication.run(CoderellaApplication.class, args);
 	}
 
+	@Bean
+	CommandLineRunner run(UserRepository userRepository, PasswordEncoder encoder) {
+		return args -> {
+			if (!userRepository.existsByUsername("ADMIN001")) {
+				User admin = new User();
+				admin.setUsername("ADMIN001");
+				admin.setEmail("admin@gmail.com");
+				admin.setPassword(encoder.encode("admin")); // plaintext = admin
+				admin.setRole(Role.SUPER_ADMIN);
+				admin.setActive(true);
+				admin.setProfileCompleted(true);
+				userRepository.save(admin);
+			}
+		};
+	}
 }
