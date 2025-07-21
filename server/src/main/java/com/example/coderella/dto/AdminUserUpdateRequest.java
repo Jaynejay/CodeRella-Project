07095eab51bd1708155e3aeed791a3ab084780ea
Isuri Lokupathirage <isurim2002@gmail.com>
2025-07-21 12:06:47 +0530

    add AdminUserUpdateRequest.java file
    new file added

---
+++ server/src/main/java/com/example/coderella/dto/AdminUserUpdateRequest.java
@@ -0,0 +1,30 @@
+
+package com.example.coderellaProject.dto;
+
+import lombok.*;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class AdminUserUpdateRequest {
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String designation;
+
+    private String homeNo;
+    private String street;
+    private String city;
+    private String district;
+
+    private String accountHolderName;
+    private String accountNumber;
+    private String bankName;
+    private String branch;
+
+    private List<String> phoneNumbers;
+    private List<String> languages;
+}
